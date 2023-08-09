0f0b0171682084b6987c75425c17b3fe8d00a434
dummydevil <ahenssolution@gmail.com>
2023-08-09 14:24:03 +0530

    commit

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
-let button = document.querySelector(".drumbtn");
+var numberOfBtn = document.querySelectorAll(".drumbtn");
+var btn_w = document.querySelector(".w");
 
-button.addEventListener("click", function(){
-    alert("Button Clicked!!");
+for (var i = 0; i < numberOfBtn.length; i++) {
+    document.querySelectorAll(".drumbtn")[i].addEventListener("click", function () {
+        var button = this.textContent;
+        btnPress(button);
+    })
+}
+function play(address){
+    var audio = new Audio(address);
+    audio.play();
+}
+document.addEventListener("keypress", function(event){
+    console.log(event.key);
+    btnPress(event.key);
 })
+function btnPress(key){
+    switch(key){
+            case "w":
+                play("sounds/crash.mp3");
+                break;
+            case "a":
+                play("sounds/kick-bass.mp3");
+                break;
+            case "s":
+                play("sounds/snare.mp3");
+                break;
+            case "d":
+                play("sounds/tom-1.mp3");
+                break;
+
+            case "j":
+                play("sounds/tom-2.mp3");
+                break;
+            case "k":
+                play("sounds/tom-3.mp3");
+                break;
+            case "l":
+                play("sounds/tom-4.mp3");
+                break;
+            default:
+                break;
+    }
+}
